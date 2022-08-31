add87307f97822ce4158ed79c958a47a5feb5919
Arnaud RITTI <arnaud.ritti@adeliom.com>
2022-08-31 11:37:33 +0200

    Fixes

--- src/Controller/CategoryCrudController.php
+++ src/Controller/CategoryCrudController.php
@@ -63,8 +63,8 @@ abstract class CategoryCrudController extends AbstractCrudController
         $context = $this->container->get(AdminContextProvider::class)->getContext();
         $subject = $context->getEntity();
 
-        yield IdField::new('id')->hideOnForm();
         yield FormField::addTab('easy.faq.admin.panel.information');
+        yield IdField::new('id')->hideOnForm();
         yield from $this->informationsFields($pageName, $subject);
         yield FormField::addTab('easy.faq.admin.panel.publication');
         yield from $this->seoFields($pageName, $subject);
