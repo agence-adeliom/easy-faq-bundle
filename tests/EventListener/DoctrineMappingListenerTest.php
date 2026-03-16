1c22b064293bba9970825243e4ce5851242f1c81
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2026-03-16 20:59:39 +0100

    doctrine dbal 4.x upgrade wip

--- tests/EventListener/DoctrineMappingListenerTest.php
+++ tests/EventListener/DoctrineMappingListenerTest.php
@@ -7,6 +7,7 @@ namespace Adeliom\EasyFaqBundle\Tests\EventListener;
 use Adeliom\EasyFaqBundle\EventListener\DoctrineMappingListener;
 use Adeliom\EasyFaqBundle\Tests\Fixtures\Entity\TestCategory;
 use Adeliom\EasyFaqBundle\Tests\Fixtures\Entity\TestEntry;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use PHPUnit\Framework\Attributes\CoversClass;
@@ -18,54 +19,30 @@ final class DoctrineMappingListenerTest extends TestCase
     public function testListenerMapsEntryCategoryAssociation(): void
     {
         $listener = new DoctrineMappingListener(TestEntry::class, TestCategory::class);
-        $metadata = $this->createMock(ClassMetadata::class);
-        $metadata->expects(self::exactly(2))
-            ->method('getName')
-            ->willReturn(TestEntry::class);
-        $metadata->expects(self::once())
-            ->method('hasAssociation')
-            ->with('category')
-            ->willReturn(false);
-        $metadata->expects(self::once())
-            ->method('mapManyToOne')
-            ->with([
-                'fieldName' => 'category',
-                'targetEntity' => TestCategory::class,
-                'inversedBy' => 'entries',
-            ]);
-
-        $event = $this->createMock(LoadClassMetadataEventArgs::class);
-        $event->expects(self::once())
-            ->method('getClassMetadata')
-            ->willReturn($metadata);
+        $metadata = new ClassMetadata(TestEntry::class);
+        $event = new LoadClassMetadataEventArgs($metadata, $this->createMock(EntityManagerInterface::class));
 
         $listener->loadClassMetadata($event);
+
+        $mapping = $metadata->getAssociationMapping('category');
+
+        self::assertSame('category', $mapping->fieldName);
+        self::assertSame(TestCategory::class, $mapping->targetEntity);
+        self::assertSame('entries', $mapping->inversedBy);
     }
 
     public function testListenerMapsCategoryEntriesAssociation(): void
     {
         $listener = new DoctrineMappingListener(TestEntry::class, TestCategory::class);
-        $metadata = $this->createMock(ClassMetadata::class);
-        $metadata->expects(self::exactly(2))
-            ->method('getName')
-            ->willReturn(TestCategory::class);
-        $metadata->expects(self::once())
-            ->method('hasAssociation')
-            ->with('entries')
-            ->willReturn(false);
-        $metadata->expects(self::once())
-            ->method('mapOneToMany')
-            ->with([
-                'fieldName' => 'entries',
-                'targetEntity' => TestEntry::class,
-                'mappedBy' => 'category',
-            ]);
-
-        $event = $this->createMock(LoadClassMetadataEventArgs::class);
-        $event->expects(self::once())
-            ->method('getClassMetadata')
-            ->willReturn($metadata);
+        $metadata = new ClassMetadata(TestCategory::class);
+        $event = new LoadClassMetadataEventArgs($metadata, $this->createMock(EntityManagerInterface::class));
 
         $listener->loadClassMetadata($event);
+
+        $mapping = $metadata->getAssociationMapping('entries');
+
+        self::assertSame('entries', $mapping->fieldName);
+        self::assertSame(TestEntry::class, $mapping->targetEntity);
+        self::assertSame('category', $mapping->mappedBy);
     }
 }
